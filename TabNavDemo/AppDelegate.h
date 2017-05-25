70d49f70c539322343b03034cf04ca7cd4b1d0c8
Cloudox <cloudox@foxmail.com>
2017-05-25 15:44:20 +0800

    Initial Commit

---
+++ TabNavDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabNavDemo
+//
+//  Created by csdc-iMac on 2017/5/25.
+//  Copyright © 2017年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
